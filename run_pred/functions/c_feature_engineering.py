dbf8af305e712450098a676b7c910e489b6cd692
Eryk Kaminski <e_kaminski@outlook.com>
2023-03-29 15:21:21 +0200

    Elevation categories

--- run_pred/functions/c_feature_engineering.py
+++ run_pred/functions/c_feature_engineering.py
@@ -11,4 +11,25 @@ def race_category(run):
     run['race_category_1'] = (run.new_metric < interval[0])*1
     run['race_category_2'] = ((run.new_metric > interval[0]) & (run.new_metric < interval[1]))*1
     run['race_category_3'] = (run.new_metric > interval[1])*1
+
+    return run
+
+def elevation_category(run):
+    """
+    Computes the elevation ratio of the runs and categorizes them into different
+    categories. The computed columns are:
+    elevation_m_per_km: shows how many meters of elevation are gained each kilometer.
+    elevation_category_1: runs with no elevation or a small elevation (=rolling elevation). (<9.5 m/km)
+    elevation_category_2: runs with a moderate/steep elevation. (9.5-29 m/km)
+    elevation_category_3: runs with a very steep elevation. (29-48 m/km)
+    elevation_category_4: runs with a mountainous elevation. (>48 m/km)
+    """
+
+    run['elevation_m_per_km'] = (run['elevation_gain'])/(run['distance']/1000)
+
+    run['elevation_category_1'] = (run.elevation_m_per_km < 9.5)*1
+    run['elevation_category_2'] = ((run.elevation_m_per_km < 29 )&(run.elevation_m_per_km <= 9.5))*1
+    run['elevation_category_3'] = ((run.elevation_m_per_km < 48 )&(run.elevation_m_per_km <= 29))*1
+    run['elevation_category_4'] = (run.elevation_m_per_km > 48)*1
+
     return run
