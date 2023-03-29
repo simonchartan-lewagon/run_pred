09729cc995814248dc24226967092b1b1f44d11b
Thomas Puech <puech81660@gmail.com>
2023-03-29 10:19:38 +0200

    debug et renomage des fonction scalling

--- run_pred/functions/d_preprocessing.py
+++ run_pred/functions/d_preprocessing.py
@@ -4,18 +4,16 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 #Standard
-def scaler_standard(X_train, X_test):
+def scaler_features(X_train, X_test):
     """
-    Cette fonction applique la méthode de mise à l'échelle des variables (scaling) sur les variables numériques d'un jeu de données.
-    Elle retourne les deux jeux de données avec les variables numériques mises à l'échelle.
+    This function applies the variable scaling method to the numerical variables of a dataset. It returns both datasets with the numerical variables scaled.
 
     Args:
-        X_train (pandas.DataFrame): Le jeu de données d'entraînement.
-        X_test (pandas.DataFrame): Le jeu de données de test.
+    X_train (pandas.DataFrame): The training dataset.
+    X_test (pandas.DataFrame): The test dataset.
 
     Returns:
-        (pandas.DataFrame, pandas.DataFrame): Un tuple de deux DataFrames contenant les jeux de données d'entraînement et de test
-        respectivement avec les variables numériques mises à l'échelle.
+    (pandas.DataFrame, pandas.DataFrame): A tuple of two DataFrames containing the scaled training and test datasets, respectively, with the numerical variables scaled
 
     """
 	# Sélection des variables numériques
@@ -42,22 +40,23 @@ def scaler_standard(X_train, X_test):
 
 
 
-def scaler_standard(y_train, y_test):
+def scaler_target(y_train, y_test):
     """
-    Cette fonction applique la méthode StandardScaler pour centrer et réduire les données numériques de la variable cible
-    y_train et y_test, et retourne un nouveau DataFrame avec les données transformées.
+    This function applies the StandardScaler method to center and scale the numerical data of the target variable y_train and y_test, and returns a new DataFrame with the transformed data.
 
     Args:
-        y_train (pandas.DataFrame): Le DataFrame contenant les données d'entraînement de la variable cible.
-        y_test (pandas.DataFrame): Le DataFrame contenant les données de test de la variable cible.
+    y_train (pandas.DataFrame): The DataFrame containing the training data of the target variable.
+    y_test (pandas.DataFrame): The DataFrame containing the test data of the target variable.
 
     Returns:
-        pandas.DataFrame: Un nouveau DataFrame avec les données numériques centrées et réduites.
+    pandas.DataFrame: A new DataFrame with the centered and scaled numerical data.
     """
+    df_y_train = pd.DataFrame(y_train).reset_index().drop(columns='index')
+    df_y_test = pd.DataFrame(y_test).reset_index().drop(columns='index')
 
-	# Application de la méthode StandardScaler aux variables numériques
+    # Application de la méthode StandardScaler aux variables numériques
     scaler = StandardScaler()
-    y_train_scaller = pd.DataFrame(scaler.fit_transform(y_train),columns= y_train.columns)
-    y_test_scaller = pd.DataFrame(scaler.transform(y_test), columns= y_test.columns)
+    y_train_scaller = pd.DataFrame(scaler.fit_transform(df_y_train),columns=['time'])
+    y_test_scaller = pd.DataFrame(scaler.transform(df_y_test),columns=['time'])
 
     return y_train_scaller, y_test_scaller
